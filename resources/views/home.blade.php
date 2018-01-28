e2fb28413d11d742216b82965da7bbf95501a9ec
DESKTOP-T315O7D\David <davidtopbing@gmail.com>
2018-01-28 16:36:29 +0700

    delete css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ URL:assets/my.css }}">
     <title>dav.wtf</title>
 </head>
 <body>
