f57009c8b47625c3b0c110f05e2d812c98e0a894
DESKTOP-T315O7D\David <davidtopbing@gmail.com>
2018-01-28 16:35:32 +0700

    change home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL:assets/my.css }}">
+    <title>dav.wtf</title>
+</head>
+<body>
+
+    <div>
+        <h1>David Tobing</h1>
+    </div>
+    
+</body>
+</html>
